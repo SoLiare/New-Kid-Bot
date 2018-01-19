8e6ae8d5668c2a0e7608fd803fb11f1ae4bc1272
SoLiare <rawissara.r@ku.th>
2018-01-19 22:39:23 +0700

    Reply when mentioned

--- newKid.js
+++ newKid.js
@@ -1,14 +1,20 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+
 const botNewKid  = new Discord.Client();
 
-botNewKid.on('ready', () => {
-    console.log(':neutral_face:');
+const TOKEN = "Mzk3NDAzNTIzNTA1MDYxODg5.DSwqbQ.Q5MNmQlUJ1zV2dN-iPCd2CTBF9M";
+
+botNewKid.on("ready", () => {
+    console.log(":neutral_face:");
 });
 
-botNewKid.on('message', message => {
-    if(message.content === 'Hi'){
-        message.reply(':slight_smile:');
+botNewKid.on("message", message => {
+    if(message.author.equals(botNewKid.user)) return;
+    else {
+        if(message.isMentioned(botNewKid.user)) {
+            message.channel.sendMessage(":neutral_face:");
+        }
     }
 });
 
-botNewKid.login('Mzk3NDAzNTIzNTA1MDYxODg5.DSwqbQ.Q5MNmQlUJ1zV2dN-iPCd2CTBF9M');
+botNewKid.login(TOKEN);
